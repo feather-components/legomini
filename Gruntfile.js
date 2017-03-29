af1d2af36f82f48e1c1c970d4b110506fa0f5abb
彭志亮 <EX-pengzhiliang001@pingan.com.cn>
2017-03-29 18:35:02 +0800

    tijiao

---
+++ Gruntfile.js
@@ -0,0 +1,90 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        copy: {
+            move: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/js/',
+                    src: 'rem.js',
+                    dest: 'build/js/'
+                },{
+                    expand: true,
+                    cwd: 'src/js/',
+                    src: 'rem.js',
+                    dest: 'dist/js/'
+                }]
+            }
+        },
+
+        less: {
+            release: {
+                options: {
+                    cleancss: false // 压缩css文件
+                },
+                files: [
+                    {
+                        expand:true,
+                        cwd:'src/css/',
+                        src:'legomini.less',
+                        dest: 'build/css/',
+                        ext: '.css'
+                    },
+                    {
+                        expand:true,
+                        cwd:'src/css/components/',
+                        src:'*.less',
+                        dest: 'build/css/components',
+                        ext: '.css'
+                    },
+                    {
+                        expand:true,
+                        cwd:'src/css/',
+                        src:'style.less',
+                        dest: 'build/css/',
+                        ext: '.css'
+                    }
+                ]
+            },
+        },
+        cssmin:{
+            target: {
+                files: [
+                    {
+                        expand:true,
+                        cwd:'build/css/',
+                        src:'legomini.css',
+                        dest: 'dist/css/',
+                        ext: '.min.css'
+                    },
+                    {
+                        expand:true,
+                        cwd:'build/css/components/',
+                        src:'*.css',
+                        dest: 'dist/css/components/',
+                        ext: '.min.css'
+                    }
+                ]
+            }
+        },
+        watch: {
+            start: {
+                files: ['src/css/*/*.less','src/css/style.less',  'src/style/*.css','src/js/*.js', 'examples/*.*'],
+                tasks: ['less', 'cssmin']
+            }
+        }
+
+    });
+
+    grunt.event.on('watch', function(action, filepath, target) {
+        grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+    });
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['less:release','cssmin:target','copy:move','watch']);
+    // grunt.registerTask('release', ['clean:release', 'copy:release', 'less:release', 'cssmin:release']);
+};
